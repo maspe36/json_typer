8696b3a681226b69d86a8c0fd0ce91b10bd7bafe
Sam Privett <privett.sam@gmail.com>
2018-12-01 14:25:58 -0500

    Fixed the import error in the pip dist

--- json_typer/__init__.py
+++ json_typer/__init__.py
@@ -1,3 +1,3 @@
-from io import exportJSON, loadJSON, dictToObjects, isDecodable, createObject
-from serializables.serializable import Serializable
-from serializables.type_serializable import TypeSerializable
+from json_typer.io import exportJSON, loadJSON, dictToObjects, isDecodable, createObject
+from json_typer.serializables.serializable import Serializable
+from json_typer.serializables.type_serializable import TypeSerializable
